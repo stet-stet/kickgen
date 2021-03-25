676615fc186434597c9c540b006435cd33cafb95
stet-stet <jayeonyi@snu.ac.kr>
2021-03-25 14:20:22 +0000

    Clean up ignored files

--- preprocess1.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root,dirs,files in os.walk("dataset"):
- for file in files:
-  if not file.endswith(".wav"):
-   os.remove(root+'/'+file)
-   print("removed "+root+'/'+file)
--- preprocess2.sh
+++
@@ -1,6 +0,0 @@
-shopt -s globstar nullglob
-for f in dataset/**/*.wav
-do
-  ffmpeg -i "$f" -acodec pcm_s16le -ar 44100 -ac 1 "${f%.wav}.new.wav"
-  mv -f "${f%.wav}.new.wav" "$f"
-done
